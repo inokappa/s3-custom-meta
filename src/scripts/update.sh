6fd02b9c9ad2c96e8e633308ee18c8861cb12a11
inokappa <inokara@gmail.com>
2021-01-03 22:44:23 +0900

    update

--- src/scripts/update.sh
+++ src/scripts/update.sh
@@ -2,16 +2,20 @@ Update() {
   echo "${PARAM_ENDPOINT}"
   OPTIONS=""
   if [ -n "${PARAM_ENDPOINT}" ];then
-    OPTIONS="--endpoint-url ${PARAM_ENDPOINT}"
+    aws --endpoint-url "${PARAM_ENDPOINT}" s3api copy-object \
+      --bucket "${PARAM_BUCKET}" \
+      --copy-source "${PARAM_BUCKET}/${PARAM_OBJECT_PATH}" \
+      --key "${PARAM_OBJECT_PATH}" \
+      --metadata-directive REPLACE \
+      --metadata "${PARAM_META_DATAS}" 
+  else
+    aws s3api copy-object \
+      --bucket "${PARAM_BUCKET}" \
+      --copy-source "${PARAM_BUCKET}/${PARAM_OBJECT_PATH}" \
+      --key "${PARAM_OBJECT_PATH}" \
+      --metadata-directive REPLACE \
+      --metadata "${PARAM_META_DATAS}" 
   fi
-
-  echo "${OPTIONS}"
-  aws --debug "${OPTIONS}" s3api copy-object \
-    --bucket "${PARAM_BUCKET}" \
-    --copy-source "${PARAM_BUCKET}/${PARAM_OBJECT_PATH}" \
-    --key "${PARAM_OBJECT_PATH}" \
-    --metadata-directive REPLACE \
-    --metadata "${PARAM_META_DATAS}" 
 }
 
 # Will not run if sourced for bats-core tests.
